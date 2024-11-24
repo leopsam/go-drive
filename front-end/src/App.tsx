144564b0681d598dfa80dca9616e1d8e12ea468c
leopsam <leonardo.lps@outlook.com.br>
2024-11-24 01:28:29 -0300

    feat: create initial RequestTrip screen layout

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RequestTrip from "./pages/RequestTrip";
+import "./styles/reset.css";
+import "./styles/global.css";
 
 function App() {
-  return (   
+  return (    
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<RequestTrip />} />
         </Routes>
       </BrowserRouter>
+         
     );  
 }
 
--- front-end/src/pages/RequestTrip/index.tsx
+++ front-end/src/pages/RequestTrip/index.tsx
@@ -1,68 +1,50 @@
-import styled from "styled-components";
+import { Container, Info, Form, InputItem, ButtonForm, TitleContainer, ButtonHistory} from "./styles";
+import { useState } from 'react';
 
 export default function Home() { 
+  const [customerId, setCustomerId] = useState('');
+  const [origin, setOrigin] = useState('');
+  const [destination, setDestination] = useState('');  
+
   return (
-      <ContainerBody>
-        <div>          
-          <Container>
-            <p>BEM VINDO</p>
-            <h1>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nunc feugiat ligula, vitae pulvinar turpis magna vel lacus. Sed non urna id libero ultrices faucibus. Aenean convallis sapien ac tortor consequat, in mollis lacus aliquet. Donec at erat a enim luctus vehicula. Vivamus quis eros sit amet mi suscipit cursus. Fusce viverra feugiat turpis, at convallis lacus rhoncus vel. Nulla facilisi. Praesent eget sollicitudin elit, non laoreet purus.
-                Suspendisse potenti. Integer venenatis, orci a laoreet tristique, libero urna venenatis nunc, nec rhoncus erat nisl nec lorem. Sed vel orci quis enim suscipit scelerisque. Maecenas ut lacinia justo, ac aliquet risus. Nulla facilisi. Etiam id sem a libero tincidunt vehicula. Nam faucibus ex vel velit tempor, sed pretium nunc posuere. Ut fringilla risus sed massa tristique, sit amet efficitur libero fringilla. Mauris tincidunt libero sed ultricies accumsan. Aliquam nec sapien nec odio accumsan volutpat non nec est. Cras feugiat leo sit amet velit vehicula, a consectetur enim rhoncus.
-            </h1>
-            <button>
-              ENVIAR
-            </button>
-          </Container>
-        </div>
-      </ContainerBody>  
+    <Container> 
+      <Info>
+        <TitleContainer>Bem vindo 🚗</TitleContainer>
+        <p>No projeto Go Drive, você poderá solicitar uma viagem em um carro particular de um ponto A até um ponto B. Ao fazer uma solicitação, será necessário informar o ID do usuário, o endereço de origem e o endereço de destino para que possamos encontrar a melhor opção para você.</p>  <p>Além disso, após preencher essas informações, você poderá escolher entre algumas opções de motoristas disponíveis e visualizar os valores para sua viagem. Depois de escolher a opção ideal, você pode confirmar a sua solicitação de viagem.</p><p>Em qualquer momento, também será possível consultar o histórico das viagens realizadas, para que você tenha total controle sobre suas viagens passadas.</p>
+        <ButtonHistory>HISTÓRICO DE VIAGENS</ButtonHistory>
+      </Info>     
+      <Form>
+        <TitleContainer>Go Drive</TitleContainer>
+        <InputItem>
+          <label htmlFor="customerId">Usuário:</label>
+          <input 
+            id="customerId" 
+            value={customerId} 
+            onChange={(e) => setCustomerId(e.target.value)} 
+            placeholder="Informe o ID do usuário"
+          />
+        </InputItem>
+        <InputItem>
+          <label htmlFor="origin">Origem:</label>
+          <input 
+            id="origin" 
+            value={origin} 
+            onChange={(e) => setOrigin(e.target.value)} 
+            placeholder="Informe o endereço de origem"
+          />
+        </InputItem>
+        <InputItem>
+          <label htmlFor="destination">Destino:</label>
+          <input 
+            id="destination" 
+            value={destination} 
+            onChange={(e) => setDestination(e.target.value)} 
+            placeholder="Informe o endereço de destino"
+          />
+        </InputItem>
+        <ButtonForm>ESTIMAR VALOR DA VIAGEM</ButtonForm>
+      </Form>      
+    </Container>    
+         
   );
 }
-const ContainerBody = styled.div`
-  width: 100vw;
-`;
-const Container = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  color: white;
-  background-color: #00000086;
-  padding: 20px;
-  @media (max-width: 768px) {
-    width: 80%;
-  }
-
-  h1 {
-    font-family: "Montserrat", sans-serif;
-    font-size: 16px;
-    color: #ffffff;
-    @media (max-width: 768px) {
-      font-size: 20px;
-      line-height: 20px;
-    }
-  }
-  p {
-    font-family: "Montserrat", sans-serif;
-    font-size: 16px;
-    color: #ffffff;   
-  }
-  button {
-    background-color: #d6a233;
-    color: #dadada;
-    border: none;
-    border-radius: 4px;
-    padding: 10px 20px;
-    margin: 0 30px;
-    font-family: "Montserrat", sans-serif;
-    font-size: 14px;
-    line-height: 20px;
-    color: #ffffff;
-    cursor: pointer;
-    @media (max-width: 768px) {
-      font-size: 12px;
-      line-height: 20px;
-    }
-  }
-`;
